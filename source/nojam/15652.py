f2fb0cc1f40b1e3810c2add43a108bde405994cc
Orb_H <orb.cov@gmail.com>
2020-03-30 12:44:43 +0900

    add 15652

---
+++ source/nojam/15652.py
@@ -0,0 +1,11 @@
+def l(a, n, m):
+    if len(a) == m:
+        print(' '.join(str(x) for x in a))
+    else:
+        for i in range(a[-1] - 1, n):
+            l(a + [i + 1], n, m)
+
+
+n, m = map(int, input().split())
+for i in range(n):
+    l([i + 1], n, m)
