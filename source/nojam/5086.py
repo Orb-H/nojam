20ddd8765396172f0a124da7998acc3320932d84
Orb_H <orb.cov@gmail.com>
2020-04-02 00:27:57 +0900

    add 5086

---
+++ source/nojam/5086.py
@@ -0,0 +1,10 @@
+while True:
+    a, b = map(int, input().split())
+    if a + b == 0:
+        break
+    if b % a == 0:
+        print('factor')
+    elif a % b == 0:
+        print('multiple')
+    else:
+        print('neither')
