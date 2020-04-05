042eeaeb9d8c82967b5dfc6c723523da4040772b
Orb_H <orb.cov@gmail.com>
2020-04-05 18:12:40 +0900

    fix wrong link generation in search

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,3 +1,8 @@
+---
+layout: null;
+---
+var baseurl="{{ site.baseurl }}";
+
 (function () {
 	function getQueryVariable(variable) {
 		var query = window.location.search.substring(1),
@@ -66,7 +71,7 @@
 					contentPreview = getPreview(query, item.content, 170),
 					titlePreview = getPreview(query, item.title);
 
-				resultsHTML += "<li><h4><a href='{{ site.baseurl }}" + item.url.trim() + "'>" + titlePreview + "</a></h4><p><small>" + contentPreview + "</small></p></li>";
+				resultsHTML += "<li><h4><a href='"+baseurl + item.url.trim() + "'>" + titlePreview + "</a></h4><p><small>" + contentPreview + "</small></p></li>";
 			});
 
 			searchResultsEl.innerHTML = resultsHTML;
